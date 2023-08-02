bad5f51534d1e15abb1ac3e1453f192fa8ee2229
Kun-777 <alex.fulin.jiang@icloud.com>
2023-08-01 23:55:05 -0500

    disabled react devtools for production; changed static JPG files to jpg

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AuthProvider } from './context/AuthProvider';
 import { CartProvider } from './context/CartProvider';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <AuthProvider>
